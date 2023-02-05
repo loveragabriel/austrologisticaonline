a744097ccde83ee5064c87def35862144ebe0e1d
Gabriel <loveragabriel20@gmail.com>
2023-02-04 22:59:21 -0300

    js working until retrive the id row for call import function

--- script/home.js
+++ script/home.js
@@ -41,7 +41,6 @@ class Order {
 //Función constiene constructo y uso de métodos 
 const createNewOrder = (e) => {
     e.preventDefault() 
-
   //Use de constructor
   let newOrder = new Order(get_shipper.value, 
     get_order.value,
@@ -60,25 +59,85 @@ const createNewOrder = (e) => {
 
     } else { newOrder.getUnitValue();
       ordersArray.push(newOrder);
+      newOrder.getUnitValue()
       console.log(ordersArray);
     // creo la etiqueta que voy a incluir en el html
     // utilizo innerHTML para definir que voy a incluir en la tag
     // selecciono la constante que me llama el id donde voy a incluir la nueva etiqueta y uso append a prepend 
+    
     document.getElementById('orders-msj').style.display = 'none'
-    const newRow = document.createElement('tr')
-    newRow.innerHTML = `<td>${get_order.value}</td>
+    const newRow = document.getElementById('new-col');
+    const getTable = document.querySelector('table');
+    newRow.innerHTML += `<tr>
+                        <td>${get_order.value}</td>
                         <td>${get_shipper.value}</td>
                         <td>${get_fob.value}</td>
                         <td>${get_freight.value}</td>
                         <td>${get_insurance.value}</td>
                         <td>${get_netkg.value}</td>
-                        <td><button id="delete-order" value='${get_order.value}' class='btn btn-danger'>x</button></td>
+                        <td>${newOrder.getUnitValue().toFixed(2)}</td>
+                        <td><button id="delete" value='${get_order.value}' class='btn btn-danger btn-sm'>Eliminar</button></td>
+                        <td><button id="importar" value='${get_order.value}' class='btn btn-success btn-sm'>Calcular</button></td>
+                        </tr>
     `
-    inner_col.appendChild(newRow);
+
+
+
     const dataOrders =  localStorage.setItem(`${get_order.value}`, JSON.stringify(newOrder));
+    
+   
+
+
+
+    getTable.addEventListener('click',(e)=>{
+        //  console.log(ordersArray)
+        //  let getId = document.getElementById(`impor-row${get_order.value}}`);
+        //  console.log(getId);
+        // alert(e.srcElement.id); 
+
+        {/* Con este obtengo el element actual
+      let btn = e.target
+      btn.closest('tr').remove();
+        con este obtengo el id del botton que seleccione
+        btn.closest('button').id;
+
+      */}
+
+      let btn = e.target
+ const deleteRow =(e)=>{
+  btn.closest('tr').remove()
+ }   
+
+ const calcularImpo=(e)=>{
+  // this code return the id value but the all row
+  //let imporId = btn.value 
+  let imporId = btn.value
+
+   const getOrder = localStorage.getItem(`${imporId}`);
+     console.log(JSON.parse(getOrder));
+    // location.href = '/pages/dash.html';
+
+}
+ 
+ 
+
+      let idBtn = btn.closest('button').id; 
+      (idBtn==='delete'? deleteRow() : calcularImpo())
+
+        
+       // let getId = document.getElementById(`${e.srcElement.id}`);
+       // console.log(getId);
+
+      })
+
   }
+
+  
  
 }
+
+
+
 
 
 
@@ -87,6 +146,5 @@ function eliminarOrden(){
 }
 
 create_order.onclick=createNewOrder; 
-document.getElementById('delete-order').onclick=eliminarOrden; 
 
 
